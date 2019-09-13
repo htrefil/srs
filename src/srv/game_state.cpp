aa56f83bdee1530674716df712e8f3431152761a
Jan Trefil <8711792+htrefil@users.noreply.github.com>
2019-09-13 23:05:23 +0200

    Better initialization of guns

--- src/srv/game_state.cpp
+++ src/srv/game_state.cpp
@@ -3,10 +3,7 @@
 namespace srv {
 
 game_state_spawned::game_state_spawned() : armor(proto::armor::BLUE), armor_health(100), gun(proto::gun::PISTOL), health(100), max_health(100), life_sequence(100) {
-	struct {
-		proto::gun gun;
-		int32_t ammo;
-	} gun_info[] = {
+	guns = {
 		{ proto::gun::SG, 0 },
 		{ proto::gun::CG, 0 },
 		{ proto::gun::RL, 0 },
@@ -14,9 +11,6 @@ game_state_spawned::game_state_spawned() : armor(proto::armor::BLUE), armor_heal
 		{ proto::gun::GL, 1 },
 		{ proto::gun::PISTOL, 40 },
 	};
-
-	for (const auto& info : gun_info)
-		guns[info.gun] = info.ammo;
 }
 
 }
